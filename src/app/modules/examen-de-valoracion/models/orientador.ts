69bbb9cfc20d91a7b4b94bd909394db414c1c8e0
Juliana Mora López <70669461+julianaml95@users.noreply.github.com>
2024-07-03 23:05:24 -0500

    Delete src/app/modules/examen-de-valoracion directory

--- src/app/modules/examen-de-valoracion/models/orientador.ts
+++
@@ -1,8 +0,0 @@
-import { Persona } from "./persona";
-
-export interface Orientador {
-    id?: number;
-    persona?: Persona;
-    rol?: string;
-    tipo?: string;
-}
--- src/app/modules/examen-de-valoracion/models/respuesta.ts
+++
@@ -1,7 +0,0 @@
-export interface Respuesta {
-    idTrabajoGrados?: number;
-    expertoEvaluaciones?: [];
-    docenteEvaluaciones?: [];
-    estadoFinalizado?: boolean;
-    observacion?: string;
-}
